b0e5e972bdff9b513ebea3e026da24873ae394f8
Christian Chiang <christiandcf@gmail.com>
2016-08-02 11:49:40 -0500

    working

--- app/src/main/java/com/christianchiang/flickerbrowser/GetRawData.java
+++ app/src/main/java/com/christianchiang/flickerbrowser/GetRawData.java
@@ -1,5 +1,15 @@
 package com.christianchiang.flickerbrowser;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by Christian on 8/1/2016.
  */
@@ -14,5 +24,91 @@ public class GetRawData {
 
     public GetRawData(String mRawURL) {
         this.mRawURL = mRawURL;
+        this.mDownloadStatus = DownloadStatus.IDLE; //By default is IDLE
+    }
+
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawURL = null;
+        this.mData = null;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+
+    public void execute(){
+        this.mDownloadStatus = DownloadStatus.PROCESSING;
+        DownloadRawData downloadRawData = new DownloadRawData();
+        downloadRawData.execute(mRawURL);
+    }
+
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+        protected void onPostExecute(String webData){
+            mData = webData;
+            Log.v(LOG_TAG, "Data returned was: " + mData);
+            if(mData == null){
+                if(mRawURL == null){
+                    mDownloadStatus = DownloadStatus.NOT_INTIALISED;
+                }else {
+                    mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+                }
+            }else{
+                //Success
+                mDownloadStatus = DownloadStatus.OK;
+            }
+        }
+
+        protected String doInBackground(String... params){
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            if(params == null)
+                return null;
+
+            try {
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if(inputStream == null){
+                    return  null;
+                }
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+
+                return  buffer.toString();
+
+            }catch(IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+
+                if(reader != null){
+                    try{
+                        reader.close();
+                    }catch (final IOException e){
+                        Log.e(LOG_TAG, "Error closing stream", e);
+
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/christianchiang/flickerbrowser/MainActivity.java
+++ app/src/main/java/com/christianchiang/flickerbrowser/MainActivity.java
@@ -1,11 +1,8 @@
 package com.christianchiang.flickerbrowser;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,14 +15,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
+
+        theRawData.execute();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
