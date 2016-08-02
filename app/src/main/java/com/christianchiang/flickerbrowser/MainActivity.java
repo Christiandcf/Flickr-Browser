4119df38bcfeb717e16c074d7d09d07dfce7a4ee
Christian Chiang <christiandcf@gmail.com>
2016-08-02 15:08:08 -0500

    in process

--- app/src/main/java/com/christianchiang/flickerbrowser/MainActivity.java
+++ app/src/main/java/com/christianchiang/flickerbrowser/MainActivity.java
@@ -15,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
+//        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
 
-        theRawData.execute();
+        GetFlickrJsonData jsonData = new GetFlickrJsonData("android,lollipop",true);
+        jsonData.execute();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
